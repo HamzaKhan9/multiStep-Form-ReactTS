f414af91bc088bae2866dea1f172eca7736d9975
HamzaKhan9 <hamzalodhi03163045212@gmail.com>
2022-10-07 12:09:41 +0500

    custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
 import React from "react";
 import "./App.css";
+import useMultistepForm from "./hooks/UseMultiStepForm";
 
 const App = () => {
-  return <div>HI</div>;
+  const { steps, currentStep } = useMultistepForm([]);
+  return (
+    <div
+      style={{
+        position: "relative",
+        background: "white",
+        border: "3px solid black",
+        padding: "2rem",
+        margin: "1rem",
+        borderRadius: ".5rem",
+        fontFamily: "Arial",
+      }}
+    >
+      <form>
+        <div style={{ position: "absolute", top: ".5rem", right: ".5rem" }}>
+          {currentStep + 1}/{steps.length}
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/hooks/useMultistepForm.ts
@@ -0,0 +1,29 @@
+import { ReactElement, useState } from "react";
+
+const useMultistepForm = (steps: ReactElement[]) => {
+  const [currentStep, setCurrentStep] = useState(0);
+  function next() {
+    setCurrentStep((i) => {
+      if (i >= steps.length - 1) return i;
+      return i + 1;
+    });
+  }
+  function back() {
+    setCurrentStep((i) => {
+      if (i <= 0) return i;
+      return i - 1;
+    });
+  }
+  function goTo(index: number) {
+    setCurrentStep(index);
+  }
+  return {
+    currentStep,
+    steps,
+    step: steps[currentStep],
+    goTo,
+    next,
+    back,
+  };
+};
+export default useMultistepForm;
